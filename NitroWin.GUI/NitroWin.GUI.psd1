794d4e93e65c65cbf56537d53790c75f05dce46b
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2024-11-10 13:30:58 +0100

    Add NitroWin.GUI module

---
+++ NitroWin.GUI/NitroWin.GUI.psd1
@@ -0,0 +1,7 @@
+@{
+    ModuleVersion = '0.1'
+    Author = 'Nitro4542'
+    Description = 'GUI for NitroWin'
+
+    RequiredModules = @('NitroWin.Helper')
+}
---
+++ NitroWin.GUI/NitroWin.GUI.psm1
@@ -0,0 +1,16 @@
+# Path to manifest
+$ManifestPath = Join-Path -Path $PSScriptRoot -ChildPath 'NitroWin.GUI.psd1'
+
+# Read manifest as hashtable
+$GuiManifest = Import-PowerShellDataFile -Path $ManifestPath
+
+# Import all functions from the Public and Private folders
+Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Public') -Filter *.ps1 | ForEach-Object { . $_.FullName }
+Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Private') -Filter *.ps1 | ForEach-Object { . $_.FullName }
+
+# Export all public functions
+$functionNames = Get-ChildItem -Path $PSScriptRoot -Filter *.ps1 -Recurse | ForEach-Object { $_.BaseName }
+Export-ModuleMember -Function $functionNames
+
+# Run initialization script
+Initialize-GUI
