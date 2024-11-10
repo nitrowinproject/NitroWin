ed8617678fa66f85ca40c06ac52ce33152124f24
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2024-11-10 15:16:07 +0100

    Add Welcome View script

---
+++ NitroWin.GUI/Private/Initialize-WelcomeView.ps1
@@ -0,0 +1,20 @@
+<#
+.SYNOPSIS
+    Initializes the WelcomeView run correctly
+
+.DESCRIPTION
+    Runs automatically when the NitroWin.GUI package is imported.
+
+.EXAMPLE
+    Initialize-WelcomeView
+#>
+
+function Initialize-WelcomeView {
+    $welcomeViewForm = Initialize-Form -xamlfile ".\NitroWin.GUI\GUI\WelcomeView.xaml"
+
+    $Global:WelcomeContinueButton.Add_Click({
+        $Global:mainWindow.Content = Initialize-LicenseView
+    })
+
+    return $welcomeViewForm
+}
