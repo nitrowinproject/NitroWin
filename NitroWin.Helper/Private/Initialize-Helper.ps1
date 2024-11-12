2e7ce8a021f266f58d7e690803a75f3c07b3c199
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2024-11-12 12:43:43 +0100

    Update Helper

--- NitroWin.Helper/Private/Initialize-Helper.ps1
+++ NitroWin.Helper/Private/Initialize-Helper.ps1
@@ -12,4 +12,9 @@
 function Initialize-Helper {
     [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
     [System.Windows.Forms.Application]::EnableVisualStyles();
+
+    # Enable TLS 1.2
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+    Set-ExecutionPolicy Unrestricted -Scope Process -Force
 }
