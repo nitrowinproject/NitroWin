af87a925d280039ccaf6dc1a02a44a98625fbdc3
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2024-11-19 14:45:31 +0100

    Remove version checking

--- src/NitroWin.Installer/Private/Initialize-Installer.ps1
+++ src/NitroWin.Installer/Private/Initialize-Installer.ps1
@@ -10,11 +10,5 @@
 #>
 
 function Initialize-Installer {
-    if ($env:NitroWinVersion -ge $Global:NitroWinVersion) {
-        $result = Show-Prompt -message "Latest version already installed. Continue?" -title "Latest version already installed" -buttons YesNo -icon Question
-
-        if (-Not ($result -eq 'Yes')) {
-            Exit 0
-        }
-    }
+    
 }
