b38565e05d44df21df63449e18ffa1bf4091a0c0
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2024-11-19 15:49:04 +0100

    Add Export-AnswerFile

---
+++ src/NitroWin.Helper/Public/Export-AnswerFile.ps1
@@ -0,0 +1,17 @@
+<#
+.SYNOPSIS
+    Copies the autounattend.xml file from NitroWin to a specified installation media
+
+.PARAMETER drive
+    The drive letter of your installation media
+
+.EXAMPLE
+    Export-AnswerFile D:\
+#>
+
+function Export-AnswerFile {
+    param (
+        [string]$drive
+    )
+    Copy-Item -Path ".\assets\autounattend\autounattend.xml" -Destination $drive
+}
