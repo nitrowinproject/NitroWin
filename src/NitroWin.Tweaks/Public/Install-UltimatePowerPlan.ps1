7e8e3ad510f1e9209f52ae073c0823d2bccce946
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2024-11-22 16:17:17 +0100

    Remove the power plan check because it does not work

--- src/NitroWin.Tweaks/Public/Install-UltimatePowerPlan.ps1
+++ src/NitroWin.Tweaks/Public/Install-UltimatePowerPlan.ps1
@@ -1,6 +1,6 @@
 <#
 .SYNOPSIS
-    Installs the Ultimate Power Plan if not already installed
+    Installs the Ultimate Power Plan
 
 .DESCRIPTION
     Uses the following GUID: e9a42b02-d5df-448d-aa00-03f14749eb61
@@ -10,7 +10,5 @@
 #>
 
 function Install-UltimatePowerPlan {
-    if (-Not (Find-InstalledPowerPlan -guid "e9a42b02-d5df-448d-aa00-03f14749eb61")) {
-        Invoke-Expression "powercfg /duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61"
-    }
+    Invoke-Expression "powercfg /duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61"
 }
