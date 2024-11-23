600c5d93df0d549ca2e084e16549866b9a491a25
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2024-11-23 11:17:27 +0100

    Add Disable-PT

---
+++ src/NitroWin.Tweaks/Public/Disable-PT.ps1
@@ -0,0 +1,15 @@
+<#
+.SYNOPSIS
+    Disables performance tracking
+
+.EXAMPLE
+    Disable-PT
+#>
+
+function Disable-SMBThrottling {
+    $regpath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WDI\{9c5a40da-b965-4fc3-8781-88dd50a6299d}"
+
+    Test-RegistryPath -path $regpath
+
+    Set-ItemProperty -Path $regpath -Name ScenarioExecutionEnabled -Value 0
+}
