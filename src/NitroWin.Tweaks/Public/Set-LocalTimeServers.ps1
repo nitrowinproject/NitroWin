9cea38aabeacc8f2d5ab6de58bbc3b37a02452d7
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2024-11-23 12:16:12 +0100

    Improve script

--- src/NitroWin.Tweaks/Public/Set-LocalTimeServers.ps1
+++ src/NitroWin.Tweaks/Public/Set-LocalTimeServers.ps1
@@ -11,7 +11,7 @@ function Set-LocalTimeServers {
 
     $servers = "0.$lang.pool.ntp.org 1.$lang.pool.ntp.org 2.$lang.pool.ntp.org 3.$lang.pool.ntp.org"
 
-    Invoke-Expression "w32tm /config /syncfromflags:manual /manualpeerlist:`"$servers`""
-    Invoke-Expression "w32tm /config /update"
-    Invoke-Expression "w32tm /resync"
+    Start-Process -wait -FilePath "w32tm.exe" -ArgumentList "/config /syncfromflags:manual /manualpeerlist:`"$servers`""
+    Start-Process -wait -FilePath "w32tm.exe" -ArgumentList "/config /update"
+    Start-Process -wait -FilePath "w32tm" -ArgumentList "/resync"
 }
