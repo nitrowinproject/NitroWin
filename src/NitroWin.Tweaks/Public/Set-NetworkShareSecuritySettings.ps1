b60c00cc9bea607ad04444018812cf4a0a4765f2
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2024-11-23 09:35:02 +0100

    Fix

--- src/NitroWin.Tweaks/Public/Set-NetworkShareSecuritySettings.ps1
+++ src/NitroWin.Tweaks/Public/Set-NetworkShareSecuritySettings.ps1
@@ -7,8 +7,8 @@
 #>
 
 function Set-NetworkShareSecuritySettings {
-    $regpath = "HKLM\SYSTEM\CurrentControlSet\Services\LanManServer\Parameters"
-    $regpath2 = "HKLM\SYSTEM\CurrentControlSet\Control\Lsa"
+    $regpath = "HKLM:\SYSTEM\CurrentControlSet\Services\LanManServer\Parameters"
+    $regpath2 = "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa"
 
     Set-ItemProperty -Path $regpath -Name RestrictNullSessAccess -Value 1
     Set-ItemProperty -Path $regpath2 -Name RestrictAnonymous -Value 1
