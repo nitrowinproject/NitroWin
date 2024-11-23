caa498818ab28096014cb05eb19ec7d6d6d1f8a4
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2024-11-23 14:24:30 +0100

    Fix formatting

--- assets/firefox/user.js
+++ assets/firefox/user.js
@@ -4,7 +4,7 @@
 */
 
 // New tab/homepage settings
-user_pref("browser.startup.page",  1);
+user_pref("browser.startup.page", 1);
 user_pref("browser.startup.homepage", "about:home");
 
 user_pref("browser.newtabpage.enabled", true);
