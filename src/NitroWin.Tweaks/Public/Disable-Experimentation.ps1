9cceae7a4194f00031ea7bb98d6cc4e676103b1e
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2024-11-23 11:09:04 +0100

    Add Disable-Experimentation

---
+++ src/NitroWin.Tweaks/Public/Disable-Experimentation.ps1
@@ -0,0 +1,13 @@
+<#
+.SYNOPSIS
+    Disables Experimentation
+
+.EXAMPLE
+    Disable-Experimentation
+#>
+
+function Disable-Experimentation {
+    $regpath = "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\System\AllowExperimentation"
+
+    Set-ItemProperty -Path $regpath -Name value -Value 0
+}
