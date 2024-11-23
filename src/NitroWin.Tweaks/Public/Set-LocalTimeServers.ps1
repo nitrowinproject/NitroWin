f4f95c6793ea7a8f8a043b39fadc5ba29765e62f
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2024-11-23 09:03:58 +0100

    Add Set-LocalTimeServers

---
+++ src/NitroWin.Tweaks/Public/Set-LocalTimeServers.ps1
@@ -0,0 +1,15 @@
+<#
+.SYNOPSIS
+    Sets your time servers to a pool of local time servers from ntppool.org
+
+.EXAMPLE
+    Set-LocalTimeServers
+#>
+
+function Set-LocalTimeServers {
+    $lang = Get-LangWithoutRegion
+
+    $servers = "0.$lang.pool.ntp.org 1.$lang.pool.ntp.org 2.$lang.pool.ntp.org 3.$lang.pool.ntp.org"
+
+    Invoke-Expression "w32tm /config /syncfromflags:manual /manualpeerlist:`"$servers`""
+}
