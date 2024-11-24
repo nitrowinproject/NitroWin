9e741c8e8ab2dfe7ff8558acbda08d52592bdb39
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2024-11-24 13:01:41 +0100

    Add Test-RegistryPath to Disable-Experimentation

--- src/NitroWin.Tweaks/Private/Disable-Experimentation.ps1
+++ src/NitroWin.Tweaks/Private/Disable-Experimentation.ps1
@@ -9,5 +9,7 @@
 function Disable-Experimentation {
     $regpath = "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\System\AllowExperimentation"
 
+    Test-RegistryPath -path $regpath
+
     Set-ItemProperty -Path $regpath -Name value -Value 0 -Type DWord
 }
