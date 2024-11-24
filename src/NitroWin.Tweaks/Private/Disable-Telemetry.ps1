e579873df7019e7a4d20e841ff083dbc29b14ccc
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2024-11-24 13:19:15 +0100

    Improve code quality

--- src/NitroWin.Tweaks/Private/Disable-Telemetry.ps1
+++ src/NitroWin.Tweaks/Private/Disable-Telemetry.ps1
@@ -10,88 +10,54 @@
 #>
 
 function Disable-Telemetry {
-    # Disable KMS telemetry
-    $kmsregpath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\CurrentVersion\Software Protection Platform"
-    $kmsname = "NoGenTicket"
-    $kmsvalue = "1"
-    $kmstype = "DWord"
+    $paths = @{
+        # Disable KMS telemetry
+        "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\CurrentVersion\Software Protection Platform" = @{
+            "NoGenTicket" = 1
+        }
+        # Disable customer improvement program
+        "HKLM:\SOFTWARE\Policies\Microsoft\AppV\CEIP" = @{
+            "CEIPEnable" = 0
+        }
+        "HKLM:\SOFTWARE\Policies\Microsoft\SQMClient\Windows" = @{
+            "CEIPEnable" = 0
+        }
+        # Disable typing insights
+        "HKCU:\SOFTWARE\Microsoft\Input\Settings" = @{
+            "InsightsEnabled" = 0
+        }
+        # Set maximum telemetry to 0
+        "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection" = @{
+            "MaxTelemetryAllowed" = 0
+        }
+        "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Policies\DataCollection" = @{
+            "MaxTelemetryAllowed" = 0
+        }
+        # Disable generic telemetry
+        "HKLM:\Software\Policies\Microsoft\Windows\DataCollection" = @{
+            "AllowDeviceNameInTelemetry" = 0
+        }
+        # Disable diagnostic tracking
+        "HKLM:\SYSTEM\CurrentControlSet\Control\Diagnostics\Performance" = @{
+            "DisableDiagnosticTracing" = 1
+        }
+        # Disable NVIDIA telemetry
+        "HKCU:\Software\NVIDIA Corporation\NVControlPanel2\Client" = @{
+            "OptInOrOutPreference" = 0
+        }
+        # Disable DHA
+        "HKLM:\SOFTWARE\Policies\Microsoft\DeviceHealthAttestationService" = @{
+            "EnableDeviceHealthAttestationService" = 0
+        }
+    }
 
-    Test-RegistryPath -path $kmsregpath
-
-    New-ItemProperty -Path $kmsregpath -Name $kmsname -Value $kmsvalue -PropertyType $kmstype
-
-    # Disable customer improvement program telemetry
-    $customerimprovementregpath = "HKLM:\SOFTWARE\Policies\Microsoft\AppV\CEIP"
-    $customerimprovementregpath2 = "HKLM:\SOFTWARE\Policies\Microsoft\SQMClient\Windows"
-    $customerimprovementname = "CEIPEnable"
-    $customerimprovementvalue = "0"
-    $customerimprovementtype = "DWord"
-
-    Test-RegistryPath -path $customerimprovementregpath
-
-    Test-RegistryPath -path $customerimprovementregpath2
-
-    New-ItemProperty -Path $customerimprovementregpath -Name $customerimprovementname -Value $customerimprovementvalue -PropertyType $customerimprovementtype
-    New-ItemProperty -Path $customerimprovementregpath2 -Name $customerimprovementname -Value $customerimprovementvalue -PropertyType $customerimprovementtype
+    foreach ($path in $paths.Keys) {
+        Test-RegistryPath -Path $path
+        foreach ($key in $paths[$path].Keys) {
+            Set-ItemProperty -Path $path -Name $key -Value $paths[$path][$key] -Type DWord
+        }
+    }
 
     # Disable .NET cli telemetry
     [System.Environment]::SetEnvironmentVariable("DOTNET_CLI_TELEMETRY_OPTOUT", "1", [System.EnvironmentVariableTarget]::Machine)
-
-    # Disable typing insights
-    $typinginsightsregpath = "HKCU:\SOFTWARE\Microsoft\Input\Settings"
-    $typinginsightsname = "InsightsEnabled"
-    $typinginsightsvalue = "0"
-    $typinginsightstype = "DWord"
-
-    Test-RegistryPath -path $typinginsightsregpath
-
-    New-ItemProperty -Path $typinginsightsregpath -Name $typinginsightsname -Value $typinginsightsvalue -PropertyType $typinginsightstype
-
-    # Set maximum telemetry to 0
-    $maxtelemetryregpath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection"
-    $maxtelemetryregpath2 = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Policies\DataCollection"
-    $maxtelemetryname = "MaxTelemetryAllowed"
-    $maxtelemetryvalue = "0"
-    $maxtelemetrytype = "DWord"
-
-    Set-ItemProperty -Path $maxtelemetryregpath -Name $maxtelemetryname -Value $maxtelemetryvalue -Type $maxtelemetrytype
-    Set-ItemProperty -Path $maxtelemetryregpath2 -Name $maxtelemetryname -Value $maxtelemetryvalue -Type $maxtelemetrytype
-
-    # Disable generic telemetry
-    $genertictelemetryregpath = "HKLM:\Software\Policies\Microsoft\Windows\DataCollection"
-    $genertictelemetryname = "AllowDeviceNameInTelemetry"
-    $genertictelemetryvalue = "0"
-    $genertictelemetrytype = "DWord"
-
-    Test-RegistryPath -path $genertictelemetryregpath
-
-    New-ItemProperty -Path $genertictelemetryregpath -Name $genertictelemetryname -Value $genertictelemetryvalue -PropertyType $genertictelemetrytype
-
-    # Disable diagnostic tracking
-    $diagtrackregpath = "HKLM:\SYSTEM\CurrentControlSet\Control\Diagnostics\Performance"
-    $diagtrackname = "DisableDiagnosticTracing"
-    $diagtrackvalue = "1"
-    $diagtracktype = "DWord"
-    
-    Set-ItemProperty -Path $diagtrackregpath -Name $diagtrackname -Value $diagtrackvalue -type $diagtracktype
-
-    # Disable NVIDIA telemetry
-    $nvidiaregpath = "HKCU:\Software\NVIDIA Corporation\NVControlPanel2\Client"
-    $nvidianame = "OptInOrOutPreference"
-    $nvidiavalue = "0"
-    $nvidiatype = "DWord"
-
-    Test-RegistryPath -path $nvidiaregpath
-
-    Set-ItemProperty -Path $nvidiaregpath -Name $nvidianame -Value $nvidiavalue -Type $nvidiatype
-
-    # Disable dha
-    $dharegpath = "HKLM:\SOFTWARE\Policies\Microsoft\DeviceHealthAttestationService"
-    $dhaname = "EnableDeviceHealthAttestationService"
-    $dhavalue = "0"
-    $dhatype = "DWord"
-
-    Test-RegistryPath -path $dharegpath
-
-    New-ItemProperty -Path $dharegpath -Name $dhaname -Value $dhavalue -PropertyType $dhatype
 }   
