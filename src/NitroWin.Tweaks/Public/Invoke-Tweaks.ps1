3d1923d23a267f4654ce9bbe9f97803a152a0f50
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2025-01-23 17:15:45 +0100

    Remove error protection as it doesn't work

--- src/NitroWin.Tweaks/Public/Invoke-Tweaks.ps1
+++ src/NitroWin.Tweaks/Public/Invoke-Tweaks.ps1
@@ -8,15 +8,8 @@
 
 function Invoke-Tweaks {
     $tweakFiles = Get-ChildItem -Path ".\src\NitroWin.Tweaks\Tweaks" -Filter "*.reg" -Recurse
-
-    foreach ($file in $tweakFiles) {
-        try {
-            Start-Process -FilePath "reg" -ArgumentList "import `"$($file.FullName)`"" -Wait -NoNewWindow
-        }
-        catch {
-            Show-Prompt -message "$($_.Exception.Message)" -title "Error while importing file: $($file.FullName)" -buttons OK -icon Error
-        }
-    }
+    
+    Start-Process -FilePath "reg" -ArgumentList "import `"$($file.FullName)`"" -Wait -NoNewWindow
 
     Disable-AI
     Disable-Telemetry
