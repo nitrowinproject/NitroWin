27f35fdc72ca60f47c1eac7a19526d1d1d59e2c9
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2025-01-25 12:45:03 +0100

    Add stuff to Initialize-MainView

--- src/NitroWin.GUI/Private/Initialize-MainView.ps1
+++ src/NitroWin.GUI/Private/Initialize-MainView.ps1
@@ -12,15 +12,42 @@
 function Initialize-MainView {
     $mainViewForm = Initialize-Form -xamlfile ".\src\NitroWin.GUI\GUI\MainView.xaml"
 
+    # Define elements
+    # Top part
+    $TopLogoImage = $mainViewForm.FindName("MainViewTopLogoImage")
+    $GPUOptionComboBox = $mainViewForm.FindName("MainViewGPUOptionComboBox")
+
+    # Middle part
+    $BraveCheckBox = $mainViewForm.FindName("MainViewBraveCheckBox")
+    $FirefoxCheckBox = $mainViewForm.FindName("MainViewFirefoxCheckBox")
+    $7ZipCheckBox = $mainViewForm.FindName("MainView7ZipCheckBox")
+    $WinRARCheckBox = $mainViewForm.FindName("MainViewWinRARCheckBox")
+    $KLCPCheckBox = $mainViewForm.FindName("MainViewKLCPCheckBox")
+    $VLCCheckBox = $mainViewForm.FindName("MainViewVLCCheckBox")
+
+    $WinUtilCheckBox = $mainViewForm.FindName("MainViewWinUtilCheckBox")
+    $OOSUCheckBox = $mainViewForm.FindName("MainViewOOSUCheckBox")
+    $KeePassXCCheckBox = $mainViewForm.FindName("MainViewKeePassXCCheckBox")
+    $UniGetUICheckBox = $mainViewForm.FindName("MainViewUniGetUICheckBox")
+    $NotepadPPCheckBox = $mainViewForm.FindName("MainViewNotepadPPCheckBox")
+    $PowerShell7CheckBox = $mainViewForm.FindName("MainViewPowerShell7CheckBox")
+
+    # Bottom part
+    $ContinueCurrentSystemButton = $mainViewForm.FindName("MainViewContinueCurrentSystemButton")
+    $ContinueInstallMediaButton = $mainViewForm.FindName("MainViewContinueInstallMediaButton")
+
+    # Load image in top part
     $imagePath = Join-Path -Path (Get-Location) -ChildPath "assets/logo/NitroWin.png"
     $imageSource = New-Object System.Windows.Media.Imaging.BitmapImage
     $imageSource.BeginInit()
     $imageSource.UriSource = [Uri]::new("file:///$imagePath")
     $imageSource.EndInit()
 
-    $mainViewForm.FindName("MainViewTopLogoImage").Source = $imageSource
+    $TopLogoImage.Source = $imageSource
 
-    $mainViewForm.FindName("MainViewGPUOptionComboBox").SelectedIndex = 0
+    # Set Other as default option in ComboBox
+    $GPUOptionComboBox.SelectedIndex = 0
+    
 
     return $mainViewForm
 }
