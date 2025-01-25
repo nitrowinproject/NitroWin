891dc40b21952aa616d1395ed6d276caf9a729d4
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2025-01-25 13:30:05 +0100

    Rename

--- src/NitroWin.Tweaks/Public/Add-UltimatePowerPlan.ps1
+++ src/NitroWin.Tweaks/Public/Add-UltimatePowerPlan.ps1
@@ -1,14 +1,14 @@
 <#
 .SYNOPSIS
-    Installs the Ultimate Power Plan
+    Adds the Ultimate Power Plan
 
 .DESCRIPTION
     Uses the following GUID: e9a42b02-d5df-448d-aa00-03f14749eb61
 
 .EXAMPLE
-    Install-UltimatePowerPlan
+    Add-UltimatePowerPlan
 #>
 
-function Install-UltimatePowerPlan {
+function Add-UltimatePowerPlan {
     Invoke-Expression "powercfg /duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61"
 }
