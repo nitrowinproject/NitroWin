880dbebdff5a72b0d1eca3baaab9b574aff0f93a
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2025-01-25 18:00:08 +0100

    Add more options to main file and improve it

--- NitroWin.ps1
+++ NitroWin.ps1
@@ -19,6 +19,7 @@ Import-Module ".\src\NitroWin.Tweaks\NitroWin.Tweaks.psm1" -Force
 Import-Module ".\src\NitroWin.GUI\NitroWin.GUI.psm1" -Force
 
 if ($args[0] -eq "--create-install-media") {
+    Write-Warning "If you want to use my fork of WinUtil to create a modified ISO, use --winutil."
     if ($args.Count -ge 2) {
         Write-Host "Deploying install media to drive $($args[1])"
         Deploy-InstallMedia -drive $args[1]
@@ -28,13 +29,16 @@ if ($args[0] -eq "--create-install-media") {
         Exit
     }
 }
-if ($args[0] -eq "--export-registry-tweaks") {
+elseif ($args[0] -eq "--export-registry-tweaks") {
     if ($args.Count -ge 2) {
         Merge-Tweaks -mergedFile $args[1]
     }
     else {
         Merge-Tweaks -mergedFile ".\merged.reg"
     }
+}
+elseif ($args[0] -eq "--winutil") {
+    Invoke-Expression (Invoke-WebRequest "https://raw.githubusercontent.com/Nitro4542/winutil/main/winutil.ps1")
 }
 else {
     Start-GUI
