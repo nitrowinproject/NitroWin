a75013f05f5bd66b49697c37f6ab74380c30b098
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2025-03-18 18:09:43 +0100

    Make Tweak importing functional

--- src/NitroWin/Tweaks.cs
+++ src/NitroWin/Tweaks.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace NitroWin {
     public class Tweaks {
         public static void Import() {
@@ -12,19 +14,22 @@ namespace NitroWin {
             }
         }
         private static void Download() {
-            
+            Console.WriteLine("Download comming soon. Please download the Tweaks folder manually.");
         }
         private static void Merge() {
             foreach (string file in Directory.EnumerateFiles("Tweaks", "*.reg", SearchOption.AllDirectories)) {
                 Console.WriteLine($"Importing {file}...");
+                Process.Start("reg.exe", $"import \"{file}\"");
             }
         }
         private static void Invoke() {
             foreach (string file in Directory.EnumerateFiles("Tweaks", "*.bat", SearchOption.AllDirectories)) {
                 Console.WriteLine($"Invoking {file}...");
+                Process.Start("cmd.exe", $"/c \"{file}\"");
             }
             foreach (string file in Directory.EnumerateFiles("Tweaks", "*.ps1", SearchOption.AllDirectories)) {
                 Console.WriteLine($"Invoking {file}...");
+                Process.Start("powershell.exe", $"-Command \"Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted -Force; & '{file}'\"");
             }
         }
     }
