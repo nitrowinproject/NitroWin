de63dac00b45f0497872d3bf393777bea76ca907
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2025-03-19 15:08:35 +0100

    Update Set-LocalTimeServers

--- src/NitroWin/Tweaks/System/NitroWin/System/Set-LocalTimeServers.ps1
+++ src/NitroWin/Tweaks/System/NitroWin/System/Set-LocalTimeServers.ps1
@@ -1,13 +1,11 @@
 <#
-The basis of this tweak comes from Atlas, but I modified it to give me more local time servers.
+The base idea of this tweak comes from Atlas, but I modified it to give me more local time servers.
 #>
 
-function Set-LocalTimeServers {
-    $region = ((Get-WinSystemLocale).Name).Split("-")[1].ToLower()
+$region = ((Get-WinSystemLocale).Name).Split("-")[1].ToLower()
 
-    $servers = "0.$region.pool.ntp.org 1.$region.pool.ntp.org 2.$region.pool.ntp.org 3.$region.pool.ntp.org"
+$servers = "0.$region.pool.ntp.org 1.$region.pool.ntp.org 2.$region.pool.ntp.org 3.$region.pool.ntp.org"
 
-    Start-Process -wait -FilePath "w32tm.exe" -ArgumentList "/config /syncfromflags:manual /manualpeerlist:`"$servers`""
-    Start-Process -wait -FilePath "w32tm.exe" -ArgumentList "/config /update"
-    Start-Process -wait -FilePath "w32tm" -ArgumentList "/resync"
-}
+Start-Process -wait -FilePath "w32tm.exe" -ArgumentList "/config /syncfromflags:manual /manualpeerlist:`"$servers`""
+Start-Process -wait -FilePath "w32tm.exe" -ArgumentList "/config /update"
+Start-Process -wait -FilePath "w32tm" -ArgumentList "/resync"
