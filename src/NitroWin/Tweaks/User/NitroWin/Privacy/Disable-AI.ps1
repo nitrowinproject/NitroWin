8d89f1603e17bf287db6c96d4cdb28dfd39bf079
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2025-03-19 14:56:18 +0100

    Update Disable-AI

--- src/NitroWin/Tweaks/User/NitroWin/Privacy/Disable-AI.ps1
+++ src/NitroWin/Tweaks/User/NitroWin/Privacy/Disable-AI.ps1
@@ -1,16 +1,1 @@
-<#
-.SYNOPSIS
-    Disables Copilot and Recall
-
-.EXAMPLE
-    Disable-AI
-#>
-
-function Disable-AI {
-    try {
-        Get-AppxPackage -AllUsers Microsoft.Copilot* | Remove-AppxPackage -AllUsers
-    }
-    catch {
-        Show-Prompt -message "Failed to remove Copilot." -title "Failed to remove Copilot" -buttons OK -icon Error
-    }
-}
+Get-AppxPackage -AllUsers Microsoft.Copilot* | Remove-AppxPackage -AllUsers
