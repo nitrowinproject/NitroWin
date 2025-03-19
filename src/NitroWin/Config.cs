55211daacf8d50809a79de6f9a03c97c5f196792
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2025-03-19 18:59:50 +0100

    Add initialize function

--- src/NitroWin/Config.cs
+++ src/NitroWin/Config.cs
@@ -1,7 +1,14 @@
 namespace NitroWin {
     public class Config {
-        public static void Initialize() {
-            
+        public static async Task Initialize() {
+            if (!File.Exists("config.yml")) {
+                String configUrl = "https://raw.githubusercontent.com/Nitro4542/NitroWin/v2/src/NitroWin/config.yml";
+                String workingDirectory = AppDomain.CurrentDomain.BaseDirectory;
+                await Helper.DownloadFile(configUrl, workingDirectory, "config.yml");
+
+                Console.WriteLine("Please configure your config file and re-run this executable again.");
+                Environment.Exit(0);
+            }
         }
     }
 }
