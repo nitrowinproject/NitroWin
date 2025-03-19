5b599720d7a4ac31088b0d08f73b052a49424c57
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2025-03-19 15:06:39 +0100

    Remove the rest of the PowerShell modules

--- src/NitroWin.Installer/NitroWin.Installer.psm1
+++
@@ -1,7 +0,0 @@
-# Import all functions from the Public and Private folders
-Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Public') -Filter *.ps1 | ForEach-Object { . $_.FullName }
-#Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Private') -Filter *.ps1 | ForEach-Object { . $_.FullName }
-
-# Export all public functions
-$functionNames = Get-ChildItem -Path $PSScriptRoot -Filter *.ps1 -Recurse | ForEach-Object { $_.BaseName }
-Export-ModuleMember -Function $functionNames
