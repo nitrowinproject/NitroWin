6262b5bdaa8144e31cfd5df0ee3327b92875affc
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2025-03-19 15:09:46 +0100

    Update Disable-Telemetry.ps1

--- src/NitroWin/Tweaks/System/NitroWin/Privacy/Disable-Telemetry.ps1
+++ src/NitroWin/Tweaks/System/NitroWin/Privacy/Disable-Telemetry.ps1
@@ -1,18 +1,2 @@
-<#
-.SYNOPSIS
-    Disables telemetry
-
-.EXAMPLE
-    Disable-Telemetry
-
-.NOTES
-    This does not disable a lot of telemetry. Only a few things. Usage of the REG file is recommended.
-#>
-
-function Disable-Telemetry {
-    # Disable .NET cli telemetry
-    [System.Environment]::SetEnvironmentVariable("DOTNET_CLI_TELEMETRY_OPTOUT", "1", [System.EnvironmentVariableTarget]::Machine)
-
-    # Disable PowerShell 7 Telemetry
-    [System.Environment]::SetEnvironmentVariable("POWERSHELL_TELEMETRY_OPTOUT", "1", [System.EnvironmentVariableTarget]::Machine)
-}
+[System.Environment]::SetEnvironmentVariable("DOTNET_CLI_TELEMETRY_OPTOUT", "1", [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable("POWERSHELL_TELEMETRY_OPTOUT", "1", [System.EnvironmentVariableTarget]::Machine)
