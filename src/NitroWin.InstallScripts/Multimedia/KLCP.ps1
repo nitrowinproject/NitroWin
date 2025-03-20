7b2d4603798baa1b1065a933e22760da29f04e6f
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2025-03-20 17:52:48 +0100

    Add KLCP install script

---
+++ src/NitroWin.InstallScripts/Multimedia/KLCP.ps1
@@ -0,0 +1,1 @@
+Start-Process -FilePath "winget.exe" -Wait -NoNewWindow -ArgumentList "install --id CodecGuide.K-LiteCodecPack.Mega --exact --scope machine --accept-package-agreements --accept-source-agreements --interactive"
