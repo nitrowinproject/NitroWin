a698dd7e6522fb8d6187b8245db7bc3b0a4f536e
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2025-03-20 17:53:16 +0100

    Add VLC install script

---
+++ src/NitroWin.InstallScripts/Multimedia/VLC.ps1
@@ -0,0 +1,1 @@
+Start-Process -FilePath "winget.exe" -Wait -NoNewWindow -ArgumentList "install --id VideoLAN.VLC --exact --scope machine --accept-package-agreements --accept-source-agreements --interactive"
