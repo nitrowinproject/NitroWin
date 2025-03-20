37e73df2a5acae4d0f6679c9f1e430828eb4c2d1
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2025-03-20 16:37:09 +0100

    Add stuff to Config.cs

--- src/NitroWin/Config.cs
+++ src/NitroWin/Config.cs
@@ -1,5 +1,9 @@
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
 namespace NitroWin {
     public class Config {
+        public ConfigFile parsedConfig = ParseConfig();
         public static async Task Initialize() {
             if (!File.Exists("config.yml")) {
                 String configUrl = "https://raw.githubusercontent.com/Nitro4542/NitroWin/v2/src/NitroWin/Assets/Configuration/config.yml";
@@ -7,8 +11,31 @@ namespace NitroWin {
                 await Helper.DownloadFile(configUrl, workingDirectory, "config.yml");
 
                 Console.WriteLine("Please configure your config file and re-run this executable again.");
+                Console.ReadKey();
                 Environment.Exit(0);
             }
         }
+        private static ConfigFile ParseConfig() {
+            String yml = File.ReadAllText("config.yml");
+            
+            var deserializer = new DeserializerBuilder()
+                .WithNamingConvention(UnderscoredNamingConvention.Instance)  // see height_in_inches in sample yml 
+                .Build();
+            
+            var config = deserializer.Deserialize<ConfigFile>(yml);
+            return config;
+        }
+    }
+    public class ConfigFile {
+        public Apps? Apps { get; set; }
+        public List<string>? Gpu { get; set; }
+    }
+    public class Apps {
+        public List<string>? Browser { get; set; }
+        public List<string>? Archiving { get; set; }
+        public List<string>? Multimedia { get; set; }
+        public List<string>? Communication { get; set; }
+        public List<string>? Gaming { get; set; }
+        public List<string>? Tools { get; set; }
     }
 }
