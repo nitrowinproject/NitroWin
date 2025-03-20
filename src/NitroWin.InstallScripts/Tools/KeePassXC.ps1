c8b961e6dd37945b2f1ca3afab219bd52ae92835
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2025-03-20 17:49:13 +0100

    Add KeePassXC install script

---
+++ src/NitroWin.InstallScripts/Tools/KeePassXC.ps1
@@ -0,0 +1,1 @@
+Start-Process -FilePath "winget.exe" -Wait -NoNewWindow -ArgumentList "install --id KeePassXCTeam.KeePassXC --exact --scope machine --accept-package-agreements --accept-source-agreements --interactive"
