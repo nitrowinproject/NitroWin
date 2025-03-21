3134aadffd9e47c83e570f5e801c5f3293812d25
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2025-03-21 15:01:29 +0100

    Improve code quality

--- src/NitroWin/Tweaks.cs
+++ src/NitroWin/Tweaks.cs
@@ -1,36 +1,24 @@
 namespace NitroWin {
     public class Tweaks {
         public static async Task Download() {
-            string registryUserUrl = "https://raw.githubusercontent.com/Nitro4542/NitroWin.Tweaks/main/NitroWin.Tweaks.User.reg";
-            string powershellUserUrl = "https://raw.githubusercontent.com/Nitro4542/NitroWin.Tweaks/main/NitroWin.Tweaks.User.ps1";
-            
-            string registrySystemUrl = "https://raw.githubusercontent.com/Nitro4542/NitroWin.Tweaks/main/NitroWin.Tweaks.System.reg";
-            string powershellSystemUrl = "https://raw.githubusercontent.com/Nitro4542/NitroWin.Tweaks/main/NitroWin.Tweaks.System.ps1";
-
             String workingDirectory = AppDomain.CurrentDomain.BaseDirectory;
 
-            if (File.Exists("NitroWin.Tweaks.User.reg")) {
-                File.Delete("NitroWin.Tweaks.User.reg");
-            }
+            var files = new (string Name, string Url)[] {
+                ("NitroWin.Tweaks.User.reg", "https://raw.githubusercontent.com/Nitro4542/NitroWin.Tweaks/main/NitroWin.Tweaks.User.reg"),
+                ("NitroWin.Tweaks.User.ps1", "https://raw.githubusercontent.com/Nitro4542/NitroWin.Tweaks/main/NitroWin.Tweaks.User.ps1"),
+                ("NitroWin.Tweaks.System.reg", "https://raw.githubusercontent.com/Nitro4542/NitroWin.Tweaks/main/NitroWin.Tweaks.System.reg"),
+                ("NitroWin.Tweaks.System.ps1", "https://raw.githubusercontent.com/Nitro4542/NitroWin.Tweaks/main/NitroWin.Tweaks.System.ps1")
+            };
+            
+            foreach (var file in files) {
+                string filePath = Path.Combine(workingDirectory, file.Name);
 
-            if (File.Exists("NitroWin.Tweaks.User.ps1")) {
-                File.Delete("NitroWin.Tweaks.User.ps1");
-            }
+                if (File.Exists(filePath)) {
+                    File.Delete(filePath);
+                }
 
-            if (File.Exists("NitroWin.Tweaks.System.reg")) {
-                File.Delete("NitroWin.Tweaks.System.reg");
+                await Helper.DownloadFile(file.Url, workingDirectory, file.Name);
             }
-
-            if (File.Exists("NitroWin.Tweaks.System.ps1")) {
-                File.Delete("NitroWin.Tweaks.System.ps1");
-            }
-
-            await Task.WhenAll(
-                Helper.DownloadFile(registryUserUrl, workingDirectory, "NitroWin.Tweaks.User.reg"),
-                Helper.DownloadFile(powershellUserUrl, workingDirectory, "NitroWin.Tweaks.User.ps1"),
-                Helper.DownloadFile(registrySystemUrl, workingDirectory, "NitroWin.Tweaks.System.reg"),
-                Helper.DownloadFile(powershellSystemUrl, workingDirectory, "NitroWin.Tweaks.System.ps1")
-            );
         }
     }
 }
