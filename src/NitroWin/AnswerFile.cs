6523cfedf8f9d3fbe13a16a418bb56b8c60daeb6
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2025-03-21 17:08:23 +0100

    Improvements

--- src/NitroWin/AnswerFile.cs
+++ src/NitroWin/AnswerFile.cs
@@ -5,26 +5,31 @@ namespace NitroWin {
         public static async Task WriteToFile() {
             var tweaks = await GetTweaks();
 
-            if (!File.Exists(AnswerFileName)) {
-                await Helper.DownloadFile(AnswerFileUrl, Helper.WorkingDirectory, AnswerFileName);
+            if (File.Exists(AnswerFileName)) {
+                File.Delete(AnswerFileName);
             }
+
+            await Helper.DownloadFile(AnswerFileUrl, Helper.WorkingDirectory, AnswerFileName);
 
             ConfigFile config = await Config.ParseConfig();
             string scriptContent = await ScriptGenerator.GenerateAppInstallScript(config);
             Helper.InsertIntoFile(AnswerFileName, scriptContent, "#NITROWINPOWERSHELLFIRSTUSER");
 
-            Helper.InsertIntoFile(AnswerFileName, tweaks["NitroWin.Tweaks.User.reg"], ";NITROWINREGISTRYFIRSTUSER");
-            Helper.InsertIntoFile(AnswerFileName, tweaks["NitroWin.Tweaks.User.reg"], ";NITROWINREGISTRYOTHERUSER");
+            var tweakFiles = new(string content, string insertPoint)[] {
+                (tweaks["NitroWin.Tweaks.User.reg"], ";NITROWINREGISTRYFIRSTUSER"),
+                (tweaks["NitroWin.Tweaks.User.reg"], ";NITROWINREGISTRYOTHERUSER"),
+                (tweaks["NitroWin.Tweaks.User.ps1"], "#NITROWINPOWERSHELLFIRSTUSER"),
+                (tweaks["NitroWin.Tweaks.User.ps1"], "#NITROWINPOWERSHELLOTHERUSER"),
+                (tweaks["NitroWin.Tweaks.System.reg"], ";NITROWINREGISTRYSYSTEM"),
+                (tweaks["NitroWin.Tweaks.System.ps1"], "#NITROWINPOWERSHELLSYSTEM")
+            };
 
-            Helper.InsertIntoFile(AnswerFileName, tweaks["NitroWin.Tweaks.User.ps1"], "#NITROWINPOWERSHELLFIRSTUSER");
-            Helper.InsertIntoFile(AnswerFileName, tweaks["NitroWin.Tweaks.User.ps1"], "#NITROWINPOWERSHELLOTHERUSER");
-
-            Helper.InsertIntoFile(AnswerFileName, tweaks["NitroWin.Tweaks.System.reg"], ";NITROWINREGISTRYSYSTEM");
-            Helper.InsertIntoFile(AnswerFileName, tweaks["NitroWin.Tweaks.System.ps1"], "#NITROWINPOWERSHELLSYSTEM");
+            foreach (var (content, marker) in tweakFiles) {
+                Helper.InsertIntoFile(AnswerFileName, content, marker);
+            }
         }
         public static async Task<Dictionary<string, string>> GetTweaks() {
             var tweakDictionary = await Tweaks.DownloadToDict();
-            var formattedDictionary = new Dictionary<string, string>();
 
             var files = new (string Name, string Content)[] {
                 ("NitroWin.Tweaks.User.reg", FormatRegistryFile(tweakDictionary["NitroWin.Tweaks.User.reg"])),
@@ -33,11 +38,7 @@ namespace NitroWin {
                 ("NitroWin.Tweaks.System.ps1", tweakDictionary["NitroWin.Tweaks.System.ps1"])
             };
 
-            foreach (var file in files) {
-                formattedDictionary.Add(file.Name, file.Content);
-            }
-
-            return formattedDictionary;
+            return files.ToDictionary(file => file.Name, file => file.Content);
         }
         private static string FormatRegistryFile(string fileContent) {
             int firstLineBreak = fileContent.IndexOf('\n');
