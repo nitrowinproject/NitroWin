942054003fa1fa89fc834cf83b092857f427836d
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2025-04-05 13:49:27 +0200

    Add TrustedInstaller tweak importing

--- src/NitroWin.Installer/Tweaks.cs
+++ src/NitroWin.Installer/Tweaks.cs
@@ -13,11 +13,19 @@ namespace NitroWin.Installer {
         };
         public static void ApplyUserTweaks() {
             CheckTweaks();
+            
             ApplyTweaks(tweakFiles[TweakType.User]);
         }
         public static void ApplySystemTweaks() {
             CheckTweaks();
-            ApplyTweaks(tweakFiles[TweakType.System]);
+
+            if (Environment.GetEnvironmentVariable("PROCESSOR_ARCHITECTURE") == "AMD64") {
+                ApplySystemTweaksPsExec();
+            }
+            else {
+                Console.WriteLine("WARNING: PsExec doesn't support arm-based CPUs yet. The tweaks will be imported as an admin, which may not import all tweaks as expected.");
+                ApplyTweaks(tweakFiles[TweakType.System]);
+            }
         }
         private static void CheckTweaks() {
             if (Directory.Exists(tweakPath)) {
@@ -31,6 +39,32 @@ namespace NitroWin.Installer {
             }
             else {
                 throw new FileNotFoundException("Tweaks folder not found, please re-run NitroWin.exe with internet access.", tweakPath);
+            }
+        }
+        private static void ApplySystemTweaksPsExec() {
+            string psexecPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Tools", "psexec64.exe");
+
+            if (!File.Exists(psexecPath)) {
+                throw new FileNotFoundException("PsExec not found, please re-run NitroWin.exe with internet access.", psexecPath);
+            }
+
+            foreach (string file in tweakFiles[TweakType.System]) {
+                if (file.EndsWith(".ps1")) {
+                    Process.Start(new ProcessStartInfo {
+                        FileName = psexecPath,
+                        Arguments = $"-accepteula -s -i powershell.exe -ExecutionPolicy Bypass -File \"{file}\"",
+                        UseShellExecute = true,
+                        Verb = "runas"
+                    });
+                }
+                else if (file.EndsWith(".reg")) {
+                    Process.Start(new ProcessStartInfo {
+                        FileName = psexecPath,
+                        Arguments = $"-accepteula -s -i reg.exe import \"{file}\"",
+                        UseShellExecute = true,
+                        Verb = "runas"
+                    });
+                }
             }
         }
         private static void ApplyTweaks(string[] tweaks) {
