2789fe80ba9625b45c4521ec1ed4d441eb1551dc
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2025-04-05 10:14:25 +0200

    Update AnswerFile.cs

--- src/NitroWin/AnswerFile.cs
+++ src/NitroWin/AnswerFile.cs
@@ -3,9 +3,13 @@ namespace NitroWin {
         private const string AnswerFileName = "autounattend.xml";
         private const string AnswerFileUrl = "https://raw.githubusercontent.com/Nitro4542/NitroWin/main/assets/AnswerFiles/autounattend.xml";
         public static async Task WriteToFile() {
-            if (!File.Exists(AnswerFileName)) {
-                await Helper.DownloadFile(AnswerFileUrl, Helper.NitroWinDirectory, AnswerFileName);
-            }            
+            var answerFilePath = Path.Combine(Helper.NitroWinDirectory, AnswerFileName);
+
+            if (File.Exists(answerFilePath)) {
+                File.Delete(answerFilePath);
+            }
+
+            await Helper.DownloadFile(AnswerFileUrl, Helper.NitroWinDirectory, AnswerFileName);          
         }
     }
 }
