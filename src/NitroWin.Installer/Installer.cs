cbc8b1a736cb9b23594d2667a2bfcdd54838e2bc
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2025-04-05 14:46:54 +0200

    Add logic

--- src/NitroWin.Installer/Installer.cs
+++ src/NitroWin.Installer/Installer.cs
@@ -1,7 +1,19 @@
 ﻿namespace NitroWin.Installer {
     public class Installer {
-        public static void Main() {
-            
+        public static async Task Main() {
+            Console.Title = "NitroWin Installer";
+
+            bool result = Helper.Prompt("This will heavily modify your system. Continue?");
+
+            if (!result) {
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine("Applying tweaks...");
+            Tweaks.Apply();
+
+            Console.WriteLine("Installing runtimes...");
+            await AppInstaller.InstallRuntimes();
         }
     }
 }
