1c0a260b340f619e88ecec3a77d53caef19edb21
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2025-04-09 17:54:12 +0200

    Update URLs

--- src/NitroWin/Downloader.cs
+++ src/NitroWin/Downloader.cs
@@ -8,10 +8,10 @@ namespace NitroWin {
         }
         private static async Task DownloadTweaks() {
             var files = new (string Name, string Url)[] {
-                ("NitroWin.Tweaks.User.reg", "https://raw.githubusercontent.com/Nitro4542/NitroWin.Tweaks/main/NitroWin.Tweaks.User.reg"),
-                ("NitroWin.Tweaks.User.ps1", "https://raw.githubusercontent.com/Nitro4542/NitroWin.Tweaks/main/NitroWin.Tweaks.User.ps1"),
-                ("NitroWin.Tweaks.System.reg", "https://raw.githubusercontent.com/Nitro4542/NitroWin.Tweaks/main/NitroWin.Tweaks.System.reg"),
-                ("NitroWin.Tweaks.System.ps1", "https://raw.githubusercontent.com/Nitro4542/NitroWin.Tweaks/main/NitroWin.Tweaks.System.ps1")
+                ("NitroWin.Tweaks.User.reg", "https://raw.githubusercontent.com/nitrowinproject/Tweaks/main/NitroWin.Tweaks.User.reg"),
+                ("NitroWin.Tweaks.User.ps1", "https://raw.githubusercontent.com/nitrowinproject/Tweaks/main/NitroWin.Tweaks.User.ps1"),
+                ("NitroWin.Tweaks.System.reg", "https://raw.githubusercontent.com/nitrowinproject/Tweaks/main/NitroWin.Tweaks.System.reg"),
+                ("NitroWin.Tweaks.System.ps1", "https://raw.githubusercontent.com/nitrowinproject/Tweaks/main/NitroWin.Tweaks.System.ps1")
             };
 
             foreach (var (name, url) in files) {
@@ -20,7 +20,7 @@ namespace NitroWin {
         }
         private static async Task DownloadAnswerFile() {
             const string answerFileName = "autounattend.xml";
-            const string answerFileUrl = "https://raw.githubusercontent.com/Nitro4542/NitroWin/main/assets/AnswerFiles/autounattend.xml";
+            const string answerFileUrl = "https://raw.githubusercontent.com/nitrowinproject/NitroWin/main/assets/AnswerFiles/autounattend.xml";
 
             await Helper.DownloadFile(answerFileUrl, Helper.NitroWinDirectory, answerFileName);
         }
