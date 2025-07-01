6d514cbf88001cdd37c770f69e359abcd4b10ecf
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2025-07-01 19:05:48 +0200

    Add PsExec functionality

--- src/helpers/Initialize-Environment.ps1
+++ src/helpers/Initialize-Environment.ps1
@@ -1,4 +1,5 @@
 function Initialize-Environment {
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
     Set-ExecutionPolicy Unrestricted -Scope Process -Force
+    Get-FileFromURL -url "https://live.sysinternals.com/PsExec64.exe"
 }
