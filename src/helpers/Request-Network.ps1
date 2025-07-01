7a3a615982ca12f6e9207a987ec1868134dd5cb2
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2025-07-01 20:31:47 +0200

    Add Request-Network.ps1

---
+++ src/helpers/Request-Network.ps1
@@ -0,0 +1,5 @@
+function Request-Network {
+    while (Test-Connection -ComputerName github.com -Count 1 -Quiet) {
+        Show-Prompt -message "NitroWin requires an active and unblocked network connection. Please connect to the internet and press OK to retry." -title "No network connection" -buttons OK -icon Error 
+    }
+}
