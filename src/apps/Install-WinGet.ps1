75ebc9ed6ebd82903ba95f3ba9226436d1c44472
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2025-07-02 17:16:44 +0200

    Optimize WinGet installer

--- src/apps/Install-WinGet.ps1
+++ src/apps/Install-WinGet.ps1
@@ -13,21 +13,16 @@ function Install-WinGet {
             default { "unknown" }
         }
 
-        $apiUrl = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
-        $release = Invoke-RestMethod -Uri $apiUrl
-        $dependenciesAsset = $release.assets | Where-Object { $_.name -eq "DesktopAppInstaller_Dependencies.zip" }
-        $dependencies = $dependenciesAsset.browser_download_url
-
+        $dependencies = "https://github.com/microsoft/winget-cli/releases/latest/download/DesktopAppInstaller_Dependencies.zip"
         $dependenciesArchive = Get-FileFromURL -url $dependencies
+
         Expand-Archive -Path $dependenciesArchive -DestinationPath (Get-DownloadFolder)
-        
         $files = Get-ChildItem (Join-Path -Path (Get-DownloadFolder) -ChildPath $arch)
         foreach ($file in $files) {
             Add-AppxPackage -Path $file
         }
 
-        $wingetAsset = $release.assets | Where-Object { $_.name -eq "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" }
-        $winget = $wingetAsset.browser_download_url
+        $winget = "https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
         $wingetInstaller = Get-FileFromURL -url $winget
 
         Add-AppxPackage $wingetInstaller
