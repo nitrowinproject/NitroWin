52bee379cedeba1352360cf3c9f40e5b953d2440
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2025-07-03 18:12:46 +0200

    Improve

--- src/helpers/Show-InstallError.ps1
+++ src/helpers/Show-InstallError.ps1
@@ -12,8 +12,12 @@ function Show-InstallError {
     $message = "Error while installing $name. Continue without installing?"
     $title = "Error while installing $name"
 
+    Write-Error "Error while installing $name."
+
     $prompt = Show-Prompt -message $message -title $title -buttons YesNo -icon Error
     if ($prompt -eq 'No') {
+        Write-Host "Quitting..."
         Exit 0
     }
+    Write-Host "Continuing..."
 }
