5c73d3820164393ebdc8ed13b30ad7eb5841149a
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2025-07-03 17:54:45 +0200

    Add synopsis

--- src/helpers/Clear-DownloadFolder.ps1
+++ src/helpers/Clear-DownloadFolder.ps1
@@ -1,3 +1,8 @@
 function Clear-DownloadFolder {
+    <#
+    .SYNOPSIS
+        This deletes everything in the current user's download folder.
+    #>
+    
     Get-ChildItem -Path (Get-DownloadFolder) -Include *.* -File -Recurse | ForEach-Object { $_.Delete() }
 }
