66336129f956d8aa4d87fe954b8617d87e11c9a2
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2025-07-03 18:41:55 +0200

    Make Clear-DownloadFolder prettier

--- src/helpers/Clear-DownloadFolder.ps1
+++ src/helpers/Clear-DownloadFolder.ps1
@@ -4,5 +4,5 @@ function Clear-DownloadFolder {
         This deletes everything in the current user's download folder.
     #>
 
-    Get-ChildItem -Path (Get-DownloadFolder) -Include *.* -File -Recurse | ForEach-Object { $_.Delete() }
+    Get-ChildItem -Path (Get-DownloadFolder) -File -Recurse | Remove-Item -ErrorAction SilentlyContinue
 }
