c0253960a2f443437e07e2a512c673829b656210
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2025-07-03 18:39:18 +0200

    Improve WinGet installer

--- src/apps/Install-WinGet.ps1
+++ src/apps/Install-WinGet.ps1
@@ -5,26 +5,42 @@ function Install-WinGet {
     #>
 
     if (-Not (Get-Command winget -ErrorAction SilentlyContinue)) {
-        $arch = switch ($env:PROCESSOR_ARCHITECTURE) {
-            "AMD64" { "x64" }
-            "x86"   { "x86" }
-            "ARM64" { "arm64" }
-            "ARM"   { "arm" }
-            default { "unknown" }
-        }
-
         $dependencies = "https://github.com/microsoft/winget-cli/releases/latest/download/DesktopAppInstaller_Dependencies.zip"
         $dependenciesArchive = Get-FileFromURL -url $dependencies
 
-        Expand-Archive -Path $dependenciesArchive -DestinationPath (Get-DownloadFolder)
+        try {
+            Write-Host "Extracting WinGet dependencies..."
+            Expand-Archive -Path $dependenciesArchive -DestinationPath (Get-DownloadFolder)
+            Write-Host "Extracted WinGet dependencies!"
+        }
+        catch {
+            Show-InstallError -name "WinGet dependencies"
+        }
+
         $files = Get-ChildItem (Join-Path -Path (Get-DownloadFolder) -ChildPath $arch)
         foreach ($file in $files) {
-            Add-AppxPackage -Path $file
+            try {
+                Write-Host "Installing $file..."
+                Add-AppxPackage -Path $file
+                Write-Host "Installed $file!"
+            }
+            catch {
+                Show-InstallError -name "WinGet dependencies"
+            }
         }
+
+        Write-Host "Installed WinGet dependencies!"
 
         $winget = "https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
         $wingetInstaller = Get-FileFromURL -url $winget
 
-        Add-AppxPackage $wingetInstaller
+        try {
+            Write-Host "Installing WinGet..."
+            Add-AppxPackage $wingetInstaller
+            Write-Host "Installed WinGet!"
+        }
+        catch {
+            Show-InstallError -name "WinGet"
+        }
     }
 }
