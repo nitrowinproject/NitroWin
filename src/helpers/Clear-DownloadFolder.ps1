73cfe1c7c79dcc1bec9bb28ed6ddb87bde69e97f
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2025-07-03 20:52:10 +0200

    Update Clear-DownloadFolder

--- src/helpers/Clear-DownloadFolder.ps1
+++ src/helpers/Clear-DownloadFolder.ps1
@@ -4,5 +4,9 @@ function Clear-DownloadFolder {
         This deletes everything in the current user's download folder.
     #>
 
-    Get-ChildItem -Path (Get-DownloadFolder) -File -Recurse | Remove-Item -ErrorAction SilentlyContinue
+    Get-ChildItem -Path (Get-DownloadFolder) -File -Recurse | Remove-Item -ErrorAction SilentlyContinue -Force 
+
+    Get-ChildItem -Path (Get-DownloadFolder) -Directory -Recurse |
+    Where-Object { !(Get-ChildItem -Path $_.FullName -Force) } |
+    Remove-Item -Force -Recurse -ErrorAction SilentlyContinue
 }
