abc783ca19041db25ab20a7e9a3c2f4c7355dbc1
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2025-07-05 10:58:28 +0200

    Remove WinUtil because automating it is bad

--- src/logic.ps1
+++ src/logic.ps1
@@ -10,20 +10,17 @@ Write-Host "|_| \_|_|\__|_|  \___/ \_/\_/  |_|_| |_|"
 Write-Host "`n"
 Write-Host "Watch how this script will heavily modify your Windows installation..."
 
-Write-Host "`n[1/6] Initializing environment..." -ForegroundColor Cyan
+Write-Host "`n[1/5] Initializing environment..." -ForegroundColor Cyan
 Initialize-Environment
 
-Write-Host "`n[2/6] Running WinUtil..." -ForegroundColor Cyan
-Invoke-WinUtil
-
-Write-Host "`n[3/6] Applying tweaks..." -ForegroundColor Cyan
+Write-Host "`n[2/5] Applying tweaks..." -ForegroundColor Cyan
 Invoke-Tweaks
 
-Write-Host "`n[4/6] Installing WinGet..." -ForegroundColor Cyan
+Write-Host "`n[3/5] Installing WinGet..." -ForegroundColor Cyan
 Install-WinGet
 
-Write-Host "`n[5/6] Installing Apps..." -ForegroundColor Cyan
+Write-Host "`n[4/5] Installing Apps..." -ForegroundColor Cyan
 Install-Apps
 
-Write-Host "`n[6/6] Cleaning up..." -ForegroundColor Cyan
+Write-Host "`n[5/5] Cleaning up..." -ForegroundColor Cyan
 Clear-DownloadFolder
