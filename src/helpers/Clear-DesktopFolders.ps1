962260969f5045cd10c3ad1c8e158769b6a054ca
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2025-08-30 15:00:15 +0200

    Fix formatting

--- src/helpers/Clear-DesktopFolders.ps1
+++ src/helpers/Clear-DesktopFolders.ps1
@@ -3,6 +3,7 @@ function Clear-DesktopFolders {
     .SYNOPSIS
         This deletes everything in the current user's and the public desktop folder.
     #>
+
     foreach ($path in @((Get-DesktopFolder), (Get-PublicDesktopFolder))) {
         Get-ChildItem -Path $path -File -Recurse | Remove-Item -ErrorAction SilentlyContinue -Force
 
