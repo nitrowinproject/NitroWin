21f3a2ce596d1cbd4e95205bffb98d47149803e0
nxtroox <100068674+nxtroox@users.noreply.github.com>
2026-05-15 13:59:18 +0200

    Log everything

--- src/NitroWin/Services/CommandLineService.cs
+++ src/NitroWin/Services/CommandLineService.cs
@@ -1,16 +1,19 @@
 ﻿using System.Reflection;
 using System.Resources;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using NitroWin.Models;
 
 namespace NitroWin.Services;
 
-internal sealed class CommandLineService(ResourceManager resourceManager, LogService logService, IHostApplicationLifetime lifetime) {
-    private readonly string _version = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? resourceManager.GetString("CommandLine_Unknown")!;
+internal sealed class CommandLineService(ResourceManager resourceManager, LogService logService, IHostApplicationLifetime lifetime, ILogger<CommandLineService> logger) {
+    private readonly string? _version = Assembly.GetExecutingAssembly().GetName().Version?.ToString();
     private readonly string _name = resourceManager.GetString("App_Name")!;
 
     internal void WriteBranding() {
         Console.Title = string.Join(" ", _name, _version);
+
+        if (!logger.IsEnabled(LogLevel.Information)) return;
 
         string[] branding = [
             "d8b   db d888888b d888888b d8888b.  .d88b.  db   d8b   db d888888b d8b   db",
@@ -19,15 +22,13 @@ internal sealed class CommandLineService(ResourceManager resourceManager, LogSer
             "88 V8o88    88       88    88`8b   88    88 Y8   I8I   88    88    88 V8o88",
             "88  V888   .88.      88    88 `88. `8b  d8' `8b d8'8b d8'   .88.   88  V888",
             "VP   V8P Y888888P    YP    88   YD  `Y88P'   `8b8' `8d8'  Y888888P VP   V8P",
-            Environment.NewLine,
-            resourceManager.GetString("App_Description")!,
-            string.Empty
+            resourceManager.GetString("App_Description")!
             ];
 
         foreach (var line in branding)
-            Console.WriteLine(line);
+            logger.LogInformation("{Line}", line);
 
-        logService.HelloFrom(_name, _version);
+        logService.HelloFrom(_name, _version ?? resourceManager.GetString("CommandLine_UnknownVersion")!);
     }
 
     internal CommandLineOptions ParseArguments(string[] args) {
@@ -38,7 +39,8 @@ internal sealed class CommandLineService(ResourceManager resourceManager, LogSer
         }
 
         if (args.Contains("-v") || args.Contains("--version")) {
-            Console.WriteLine(_version);
+            if (logger.IsEnabled(LogLevel.Information))
+                logger.LogInformation("{Version}", _version);
             lifetime.StopApplication();
             return new CommandLineOptions();
         }
@@ -50,7 +52,9 @@ internal sealed class CommandLineService(ResourceManager resourceManager, LogSer
     }
 
     private void WriteHelp() {
-        Console.WriteLine(resourceManager.GetString("CommandLine_Options"));
+        if (!logger.IsEnabled(LogLevel.Information)) return;
+
+        logger.LogInformation("{Options}", resourceManager.GetString("CommandLine_Options"));
 
         string[] options = [
             "-h, --help       => Print help",
@@ -60,7 +64,7 @@ internal sealed class CommandLineService(ResourceManager resourceManager, LogSer
             ];
 
         foreach (var option in options) {
-            Console.WriteLine(option);
+            logger.LogInformation("{Option}", option);
         }
     }
 }
