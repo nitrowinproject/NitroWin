989b633d9766a7e2235132c16da2acebc18a3c00
nxtroox <100068674+nxtroox@users.noreply.github.com>
2026-05-15 18:06:01 +0200

    Make Name and Author in config files optional

--- src/NitroWin/Models/ConfigBase.cs
+++ src/NitroWin/Models/ConfigBase.cs
@@ -1,6 +1,6 @@
 ﻿namespace NitroWin.Models;
 
 public abstract class ConfigBase {
-    public string? Name { get; set; }
-    public string? Author { get; set; }
+    public string? Name { get; set; } = null;
+    public string? Author { get; set; } = null;
 }
