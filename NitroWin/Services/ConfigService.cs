f196a6119908ec0e9dd8af986f70114dfa72b76f
nxtroox <100068674+nxtroox@users.noreply.github.com>
2026-06-10 21:39:33 +0200

    Improve ConfigService

--- NitroWin/Services/ConfigService.cs
+++ NitroWin/Services/ConfigService.cs
@@ -8,40 +8,25 @@ public sealed class ConfigService(IDeserializer deserializer, LogService logServ
     private AppInstallerConfig? _appInstallerConfig;
 
     internal async Task<Config> GetAsync(CancellationToken cancellationToken = default) =>
-        _config ??= await LoadAsync(Path.Combine(
+        _config ??= await LoadAsync<Config>(Path.Combine(
             AppDomain.CurrentDomain.BaseDirectory, "Configuration", "Config.yml"), cancellationToken);
 
-    private async Task<Config> LoadAsync(string path, CancellationToken cancellationToken = default) {
-        if (!File.Exists(path)) {
-            logService.NoConfigFound<Config>();
-            return new Config();
-        }
-
-        try {
-            var content = await File.ReadAllTextAsync(path, cancellationToken);
-            return deserializer.Deserialize<Config>(content);
-        } catch (Exception ex) {
-            logService.ConfigError<Config>(ex);
-            return new Config();
-        }
-    }
-
     internal async Task<AppInstallerConfig> GetAppInstallerAsync(CancellationToken cancellationToken = default) =>
-        _appInstallerConfig ??= await LoadAppInstallerAsync(Path.Combine(
+        _appInstallerConfig ??= await LoadAsync<AppInstallerConfig>(Path.Combine(
             AppDomain.CurrentDomain.BaseDirectory, "Configuration", "Apps.yml"), cancellationToken);
 
-    private async Task<AppInstallerConfig> LoadAppInstallerAsync(string path, CancellationToken cancellationToken = default) {
+    private async Task<T> LoadAsync<T>(string path, CancellationToken cancellationToken) where T : ConfigBase, new() {
         if (!File.Exists(path)) {
-            logService.NoConfigFound<AppInstallerConfig>();
-            return new AppInstallerConfig();
+            logService.NoConfigFound<T>();
+            return new T();
         }
 
         try {
             var content = await File.ReadAllTextAsync(path, cancellationToken);
-            return deserializer.Deserialize<AppInstallerConfig>(content);
+            return deserializer.Deserialize<T>(content);
         } catch (Exception ex) {
-            logService.ConfigError<AppInstallerConfig>(ex);
-            return new AppInstallerConfig();
+            logService.ConfigError<T>(ex);
+            return new T();
         }
     }
 }
